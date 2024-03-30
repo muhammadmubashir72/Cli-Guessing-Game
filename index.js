b9fc49a4785b8575fc64bfdd7569f14b6c0142c6
muhammadmubashir72 <mubashirkhi72@gmail.com>
2024-03-31 03:08:44 +0500

    I have a created cli guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const random = Math.floor(Math.random() * 10 + 1);
+console.log("\n\n\t\t\t\t-------- Welcome To Guessing Game --------\n");
+const userInput = await inquirer.prompt([
+    {
+        message: "Please guess a number between 1-10: ",
+        type: "number",
+        name: "userGuessedNumber",
+    },
+]);
+if (userInput.userGuessedNumber === random) {
+    console.log("Congrulation! You Guess a right number.");
+}
+else {
+    console.log("You Guess a wrong number.");
+}
